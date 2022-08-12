71f42656338a5649e499f18bf54af0f002890617
Breno Freitas da Silva <freitasbreno174@gmail.com>
2022-08-12 00:20:01 -0300

    f

---
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public readonly DataContext _Context;
+
+        public EventoController(DataContext context)
+        {
+            _Context = context;
+    
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _Context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _Context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+        }
+
+    }
+}
